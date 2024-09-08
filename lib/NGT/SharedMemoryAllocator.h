f4ea39140b80753fc79aeb77046c71f747be5b9b
José Magalhães <61989164+jpmag7@users.noreply.github.com>
2024-09-08 23:25:49 +0100

    Update SharedMemoryAllocator.h

--- lib/NGT/SharedMemoryAllocator.h
+++ lib/NGT/SharedMemoryAllocator.h
@@ -111,7 +111,7 @@ class SharedMemoryAllocator {
     if (msize == 0) {
       msize = NGT_SHARED_MEMORY_MAX_SIZE;
     }
-    msize = 100;
+    msize = 1000; /////////////////////////////////////////////////////// 1GB of mem increment ///////////////////////////////////////
     size_t bsize = msize * 1048576 / sysconf(_SC_PAGESIZE) + 1; // 1048576=1M
     uint64_t size = bsize * sysconf(_SC_PAGESIZE);
     MemoryManager::init_option_st option;
